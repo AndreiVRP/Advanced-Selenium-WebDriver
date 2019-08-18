4723d20914dbafb5220d58693c02d441237d3758
Andrei <andrei.hrabovich@gmail.com>
2019-08-18 19:01:44 +0300

    Base Page Object + refactoring

---
+++ BEGINNING/advanced-selenium-webdriver/src/test/java/com/herokuapp/theinternet/pages/BasePageObject.java
@@ -0,0 +1,48 @@
+package com.herokuapp.theinternet.pages;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePageObject {
+    protected WebDriver driver;
+    protected  Logger log;
+
+    public BasePageObject(WebDriver driver, Logger log) {
+        this.driver = driver;
+        this.log = log;
+    }
+
+    protected void openUrl(String url) {
+        driver.get(url);
+    }
+
+    protected WebElement find(By locator) {
+        return driver.findElement(locator);
+    }
+
+    protected void waitFor(ExpectedCondition<WebElement> condition, Integer timeOutInSeconds) {
+        timeOutInSeconds = timeOutInSeconds != null ? timeOutInSeconds : 30;
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.until(condition);
+    }
+
+    protected void waitForVisibilityOf(By locator, Integer timeOutInSeconds) {
+        waitFor(ExpectedConditions.visibilityOfElementLocated(locator), timeOutInSeconds);
+    }
+
+    public void click(By locator) {
+        waitForVisibilityOf(locator, 5);
+        find(locator).click();
+    }
+
+    public void type(String text, By locator) {
+        waitForVisibilityOf(locator, 5);
+        find(locator).sendKeys(text);
+    }
+
+}
--- BEGINNING/advanced-selenium-webdriver/src/test/java/com/herokuapp/theinternet/pages/WelcomePageObject.java
+++ BEGINNING/advanced-selenium-webdriver/src/test/java/com/herokuapp/theinternet/pages/WelcomePageObject.java
@@ -4,26 +4,23 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class WelcomePageObject {
-    private WebDriver driver;
-    private Logger log;
+public class WelcomePageObject extends BasePageObject {
 
     private String pageURL = "http://the-internet.herokuapp.com/";
     private By formAuthenticatorLinkLocator = By.linkText("Form Authenticaton");
     public WelcomePageObject(WebDriver driver, Logger log) {
-        this.driver = driver;
-        this.log = log;
+        super(driver, log);
     }
 
     public void openPage() {
         log.info("Opening page: " + pageURL);
-        driver.get(pageURL);
+        openUrl(pageURL);
         log.info("Page opened! ");
     }
 
     public LoginPage clickFormAuthenticationLink() {
         log.info("Clicking form auth link on welcome page");
-        driver.findElement(formAuthenticatorLinkLocator).click();
+        click(formAuthenticatorLinkLocator);
         return new LoginPage(driver, log);
     }
 }
