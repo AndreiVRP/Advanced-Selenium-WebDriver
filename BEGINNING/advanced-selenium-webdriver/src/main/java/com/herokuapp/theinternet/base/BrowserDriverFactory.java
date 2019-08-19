0bf5b1d3416905d737e9d5fcf051cfe07c44147b
Andrei <andrei.hrabovich.vrp@gmail.com>
2019-08-19 16:45:25 +0300

    re-updated pom.xml + re-added macOS drivers

--- BEGINNING/advanced-selenium-webdriver/src/main/java/com/herokuapp/theinternet/base/BrowserDriverFactory.java
+++ BEGINNING/advanced-selenium-webdriver/src/main/java/com/herokuapp/theinternet/base/BrowserDriverFactory.java
@@ -22,18 +22,18 @@ public class BrowserDriverFactory {
 
 		switch (browser) {
 		case "chrome":
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 			driver.set(new ChromeDriver());
 			break;
 
 		case "firefox":
-			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
 			driver.set(new FirefoxDriver());
 			break;
 
 		default:
 			System.out.println("Do not know how to start: " + browser + ", starting chrome.");
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 			driver.set(new ChromeDriver());
 			break;
 		}
